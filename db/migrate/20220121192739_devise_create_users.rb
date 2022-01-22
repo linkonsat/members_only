f3d42948b919ce77809e5374fac6011c11333c86
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-01-21 22:11:59 -0800

    Added devise gem user table.

--- db/migrate/20220121192739_devise_create_users.rb
+++ db/migrate/20220121192739_devise_create_users.rb
@@ -3,6 +3,8 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :bio
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -38,7 +40,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :users, :confirmation_token,   unique: true
+    add_index :users, :unlock_token,         unique: true
   end
 end
