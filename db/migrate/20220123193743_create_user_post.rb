2d0d4cb72d551d4470cd60f960f8c6834e9360af
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-01-23 11:38:35 -0800

    Created migration for user posts.

---
+++ db/migrate/20220123193743_create_user_post.rb
@@ -0,0 +1,8 @@
+class CreateUserPost < ActiveRecord::Migration[7.0]
+  def change
+    create_table :user_posts do |t|
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_01_21_192739) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "user_name"
     t.string "bio"
