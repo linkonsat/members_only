aa3fefa0c7f20da6ed17e7a4250f6a33b3413ce5
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-01-22 21:47:57 -0800

    Switched Databse to postgres.

--- test/controllers/mainpages_controller_test.rb
+++ test/controllers/mainpages_controller_test.rb
@@ -4,6 +4,13 @@ class MainpagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get index" do
     get "/"
-    assert_select "h1", "Welcome"
+    assert_select "h1", "Welcome to the club!"
   end
+
+  test "Should redirect to users page when sign in is selected."
+
+end
+
+test "Should redirect to the user signup page when signup button is clicked"
+end
 end
