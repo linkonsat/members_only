dc8f91a2b2fbb268cddcb4a3013a5fbe0c799d88
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-01-27 11:31:54 -0800

    Added new routes for devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'mainpages#index'
   resources :mainpages
   resources :user
-  devise_for :user
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
