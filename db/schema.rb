9e60690e437b3ea7cd640b8408e160ff67d325e6
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-02-11 09:05:22 -0800

    Fixed create posts bug.:

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_200920) do
+ActiveRecord::Schema.define(version: 2022_01_25_052648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "events", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "creator"
-    t.string "event_date"
-    t.string "event_end_date"
+  create_table "posts", force: :cascade do |t|
+    t.text "title"
+    t.text "preview"
+    t.text "body"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,8 +35,7 @@ ActiveRecord::Schema.define(version: 2022_02_01_200920) do
     t.datetime "remember_created_at", precision: 6
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
