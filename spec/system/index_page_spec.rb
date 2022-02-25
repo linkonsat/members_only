1ac7e07018fbed0f7f99711c4748d02e936ea787
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-02-24 23:17:42 -0800

    Wrote new post creation test.

--- spec/system/index_page_spec.rb
+++ spec/system/index_page_spec.rb
@@ -7,6 +7,13 @@ RSpec.describe 'MainPage Index', type: :system do
     visit "/"
     expect(page).to_not have_content("test-001@sample.com")
     end
+
+    it "Displays a post title as anonymous." do 
+      User.create(:email => 'test@example.com', :user_name => "bob", :password => 'f4k3p455w0rd')
+      Post.create(:title => "test", :preview => "test post", :body => "test post", :user_id => 19)
+      visit "/"
+      expect(page).to have_content("Anonymous")
+    end
   end
 
   describe "Event creation process" do
@@ -14,21 +21,19 @@ RSpec.describe 'MainPage Index', type: :system do
       user = User.create(:email => 'test@example.com', :user_name => "bob", :password => 'f4k3p455w0rd')
     login_as(user, :scope => :user)
       visit "/"
-      expect(page).to have_content('bob')
+      expect(page).to have_content('test@example.com')
     end
     it "Sucessfully creates a event." do 
       user = User.create(:email => 'test@example.com', :user_name => "bob", :password => 'f4k3p455w0rd')
     login_as(user, :scope => :user)
     ApplicationController.any_instance.stub(:current_user).and_return(user)
-      visit "/posts/new"
-      #click_link('create post')
-     # debugger
-      fill_in "post_title", with: "test event"
-      fill_in "post_preview", with: "test preview"
-      fill_in "post_body", with: "A test event"
-      click_on "Create Post"
-      
-      expect(page).to have_content("test event")
+    visit "/"
+      click_on 'create post'
+      page.fill_in "post_title", with: "test event"
+      page.fill_in "post_preview", with: "test preview"
+      page.fill_in "post_body", with: "A test event"
+      page.click_on "Create Post"
+      expect(page).to have_content("Post was sucessfully created!")
     end
 
   end
