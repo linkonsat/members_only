4ca6cbc5830ecae0092b2ff29f64d36fb0435fcb
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-02-28 10:53:22 -0800

    Added uniqueness validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  validate do |user|
+    errors.add :email, :not_unique, message: "This email has already been taken. Select another"
+    errors.add :user_name, :not_unique, message: "This username has already been taken. Please select another."
+  end
+  validates :email, uniqueness: true
+  validates :user_name, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
