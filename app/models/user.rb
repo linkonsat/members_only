c40f1776b6b9ede40286f3c9a64e1e62f6d16464
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-03-01 10:10:30 -0800

    Removed unused views.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true
+  validates :user_name, uniqueness: true
   validate do |user|
     errors.add :email, :not_unique, message: "This email has already been taken. Select another"
     errors.add :user_name, :not_unique, message: "This username has already been taken. Please select another."
+  #debugger
   end
-  validates :email, uniqueness: true
-  validates :user_name, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
