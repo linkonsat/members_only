f96282fd88b13581e84841c1b8b7ba10f48f25a1
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-03-01 15:00:28 -0800

    Added and passed new tests.

--- spec/system/devise_forms_spec.rb
+++ spec/system/devise_forms_spec.rb
@@ -2,19 +2,26 @@ require "rails_helper"
 require_relative "../support/devise"
 
 RSpec.describe 'Devise user forms', type: :system do 
-    describe "A new user can sucessfully register." do 
+    describe "User form interaction" do 
+        it "Sucessfully signs up a new user"  do 
         visit "/"
         click_on "Register"
         fill_in "user_user_name", with: "freya"
         fill_in "user_email", with: "freya@gmail.com"
         fill_in "user_password", with: "123456"
         fill_in "user_password_confirmation", with: "123456"
-        expect(page).to_have content("freya@gmail.com")
+        click_on "Sign up"
+        expect(page).to have_content("freya@gmail.com")
+        end
     end
 
     describe "A logged in user can sign out" do 
-        visit "/"
+        it "Sucessfully sings out a signed in user." do 
         user = User.create(:email => 'test@example.com', :user_name => "bob", :password => 'f4k3p455w0rd')
     login_as(user, :scope => :user)
+    visit "/"
+    click_on "Log out"
+    expect(page).to have_content("Log in")
+        end
     end
 end
--- spec/system/index_page_spec.rb
+++ spec/system/index_page_spec.rb
@@ -1,18 +1,11 @@
 require "rails_helper"
 require_relative "../support/devise"
 
-RSpec.describe 'MainPage Index', type: :system do
+RSpec.describe 'Index page', type: :system do
   describe 'Displays proper index page content' do
     it "Does not display a unsigned user email" do
     visit "/"
     expect(page).to_not have_content("test-001@sample.com")
-    end
-
-    it "Displays a post title as anonymous." do 
-      User.create(:email => 'test@example.com', :user_name => "bob", :password => 'f4k3p455w0rd')
-      Post.create(:title => "test", :preview => "test post", :body => "test post", :user_id => 19)
-      visit "/"
-      expect(page).to have_content("Anonymous")
     end
   end
 
