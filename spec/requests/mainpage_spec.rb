1103c8d1b0adf3d477e3b221deda3e9c523f1c0b
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-03-01 12:46:13 -0800

    Removed unneeded tests and files.

--- spec/requests/mainpage_spec.rb
+++ spec/requests/mainpage_spec.rb
@@ -1,12 +1,1 @@
 require 'rails_helper'
-RSpec.describe MainpagesController, :type => :controller do
-  render_views
-
-describe 'GET #index' do
-it 'returns http success' do
-  get :index
-  expect(response).to have_http_status(:success)
-  expect(response).to render_template("index")
-end
-end
-end
