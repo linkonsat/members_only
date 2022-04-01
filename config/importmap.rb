e51ce62f102b3fc51b902dcd1ccf308323d15204
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-03-31 20:54:12 -0700

    Added stimulus controllers.

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,11 +2,13 @@
 
 # Pin npm packages by running ./bin/importmap
 
-pin "react", to: "https://cdn.skypack.dev/react"
-pin "react-dom", to: "https://cdn.skypack.dev/react-dom"
+pin "react", to: "https://ga.jspm.io/npm:react@17.0.2/index.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@17.0.2/index.js"
 pin 'application', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
-pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
-pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
-pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin "jest", to: 'https://cdn.skypack.dev/jest';
+pin_all_from "app/javascript/components", under: "components"
+pin "jest", to: 'https://cdn.skypack.dev/jest';pin "object-assign", to: "https://ga.jspm.io/npm:object-assign@4.1.1/index.js"
+pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.20.2/index.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
