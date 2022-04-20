707293f681b6dfa4d904806827b8309e04efc702
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-04-20 10:10:04 -0700

    Updated seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,13 @@
 #   Character.create(name: "Luke", movie: movies.first)
 user_count = 0
 post_count = 0
-debugger
-5.times do
+100.times do
   user_count += 1
   User.create!(user_name: "user#{user_count}", bio: "I am user #{user_count}",
                email: "user#{user_count}@gmail.com", password: "#{user_count}12345", admin: false)
 end
 
-5.times do
+100.times do
   post_count += 1
   Post.create!(title: 'My first post!', preview: 'I....', body: 'I am a test user', user_id: post_count)
 end
