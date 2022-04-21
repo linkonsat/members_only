58cea0ff3177f1690fc861574a83df31e5d999ab
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-04-20 22:36:23 -0700

    Added unit tests and updated configuration files.

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
+FactoryBot.define do 
+  sequence :id do |n|
+    n 
+  end
+end
+
 FactoryBot.define do
+    
     factory :post do
-      sequence(:id) { |n| n }
+      id { generate :id}
       user_id { 1 }
     end
   end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,10 +5,32 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   it 'Retrieves the first 25 batch of posts' do
     create(:user)
-    create_list(:post, 25)
+    25.times do 
+    create(:post)
+    end
     posts = Post.initial_index_page_posts
     expect(posts.length).to eql(25)
+    FactoryBot.rewind_sequences
   end
-  it 'Succesfully gets multiple posts' do
+
+
+  it 'It can count the amount of index links' do
+    create(:user)
+    45.times do 
+    create(:post)
+    end
+    page_count = Post.all_posts_page_count
+    expect(page_count).to eql(1)
+    FactoryBot.rewind_sequences
+  end
+  it "Retrieves the specified post from the params parameters." do 
+    create(:user)
+    45.times do 
+      create(:post)
+    end
+    params_hash = { "page_number" => "1" }
+    retrieved_posts = Post.display_index_posts(params_hash)
+    expect(retrieved_posts.length).to eql(25)
+    FactoryBot.rewind_sequences
   end
 end
