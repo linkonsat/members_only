7930bd276b9b6d011fab8a7734ce6dc2d62dc8e4
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-04-20 19:18:42 -0700

    Added unit test for model method.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it 'Successfully gets the first post' do
+  it 'Retrieves the first 25 batch of posts' do
+    create(:user)
+    create_list(:post, 25)
+    posts = Post.initial_index_page_posts
+    expect(posts.length).to eql(25)
   end
   it 'Succesfully gets multiple posts' do
   end
