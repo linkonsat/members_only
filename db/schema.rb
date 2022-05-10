c603f80f400105783d26377964a03b1db49fb3f3
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-05-10 13:44:14 -0700

    Added rewards object for users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_02_031924) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_24_152105) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_02_031924) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "rewards", force: :cascade do |t|
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_rewards_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "user_name"
     t.string "bio"
@@ -58,4 +63,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_02_031924) do
   end
 
   add_foreign_key "posts", "users"
+  add_foreign_key "rewards", "users"
 end
