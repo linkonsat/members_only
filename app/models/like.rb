55ce035f427149294d46de45131abc7f951d3d4a
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-06-02 14:08:08 -0700

    Removed ability to double like.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,15 @@
 class Like < ApplicationRecord
     belongs_to :post
     belongs_to :user
+
+    validate :unique_like 
+
+    def unique_like 
+        post_likes = self.post.likes 
+        post_likes.each do |like|
+            if(like.user_id == self.user_id)
+                errors.add(:already_likes, "You already liked this.")
+            end
+        end
+    end
 end
