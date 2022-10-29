f9624e475485823d3b589a716605a277bbe48364
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-10-28 20:19:38 -0700

    Added test files.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  factory :message do
+    
+  end
+
+  factory :comment do
+    
+  end
+
+  factory :like do
+    
+  end
+
   factory :user do
     sequence(:email) { |n| "test-#{n.to_s.rjust(3, '0')}@sample.com" }
     password { '123456' }
